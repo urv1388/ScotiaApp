1c6f105b2369eae1dd33a686cc0d48b78c109e51
Urvish Patel <urvish.patel@rci.rogers.com>
2023-06-28 19:37:02 -0400

    Added toolbar

--- app/src/main/java/com/urvish/scotia/ui/main/MainActivity.kt
+++ app/src/main/java/com/urvish/scotia/ui/main/MainActivity.kt
@@ -3,25 +3,42 @@ package com.urvish.scotia.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.urvish.scotia.R
 import com.urvish.scotia.ui.common.theme.ScotiaAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ScotiaAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    Scaffold(topBar = {
+                        TopAppBar(title = {
+                            Text(text = stringResource(id = R.string.app_name), color = Color.White)
+                        })
+                    }) {
+                        Box(modifier = Modifier.padding(it)) {
+                            MainScreen()
+                        }
+                    }
                 }
             }
         }
--- ui-common/src/main/java/com/urvish/scotia/ui/common/theme/Theme.kt
+++ ui-common/src/main/java/com/urvish/scotia/ui/common/theme/Theme.kt
@@ -18,13 +18,16 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    surface = Purple80,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    surface = Purple40,
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,8 +44,8 @@ private val LightColorScheme = lightColorScheme(
 fun ScotiaAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
